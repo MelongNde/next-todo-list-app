8a093aebb1cdc3d7c4eea7de8817cd997b7b51df
MelongNde <nde.brice@seed120.com>
2023-07-27 15:03:19 +0100

    Testing Lottie

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,12 +1,18 @@
 
+"use client"
 import { Itask } from "../../types/tasks"
 import Task from "./Task"
+import Lottie, { LottieRefCurrentProps } from 'lottie-react'
+import animationData from './../assets/phone-animations.json'
+
+import { useRef } from 'react'
 
 interface TodoListProps {
     tasks: Itask[]
 }
 
 const TodoList: React.FC<TodoListProps> = ( { tasks } ) => {
+  const phoneRef = useRef<LottieRefCurrentProps>(null)
   return (
     <div className="overflow-x-auto">
         <table className="table w-full">
@@ -20,6 +26,16 @@ const TodoList: React.FC<TodoListProps> = ( { tasks } ) => {
             { tasks.map(task => <Task key={task.id} task={task} /> ) } 
             </tbody>
         </table>
+        <Lottie 
+          lottieRef={phoneRef} 
+          animationData={animationData} 
+          size={30}
+          onComplete={() => {
+            phoneRef.current?.setDirection(-1)
+            phoneRef.current.play()
+          }}
+          loop={true}
+        />
     </div>
   )
 }
